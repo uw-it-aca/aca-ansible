c1bd57cf8b1481bd5dcbdb7e0be2d6b5e2322c1c
Patrick Michaud <pmichaud@uw.edu>
2015-11-18 14:21:49 -0800

    Adding the permission needed to list slave host connections

--- playbooks/templates/nagios/mysql_master_check.py
+++ playbooks/templates/nagios/mysql_master_check.py
@@ -6,6 +6,7 @@
 #
 # If you are looking at this script because it failed to connect, you can run this mysql command:
 # CREATE USER '{{ nagios_mysql_user|default('nagios') }}'@'localhost' IDENTIFIED BY '{{ mysql_password }}';
+# GRANT PROCESS ON *.* TO '{{ nagios_mysql_user|default('nagios') }}'@'localhost';
 #
 ######
 
