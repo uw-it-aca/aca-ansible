3e462171a4a3708cc3fd7af2f1dd14ac5b365e5d
Fang Lin <fanglin@uw.edu>
2016-07-24 13:42:49 -0700

    Add bridge setting and restclients/hrpws.

---
+++ playbooks/templates/bridge/project_settings.py
@@ -0,0 +1,100 @@
+# Settings for Bridge project.
+# Django 1.9.6.
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+TIME_ZONE = 'America/Los_Angeles'
+
+STATIC_URL = '/data/bridge/static/'
+
+# Assign rather than append since order is significant
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.PersistentRemoteUserMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'userservice.user.UserServiceMiddleware',
+]
+
+INSTALLED_APPS += (
+    'restclients',
+    # 'authz_group',
+    # 'supporttools',
+    'sis_provisioner',
+)
+
+AUTHZ_GROUP_BACKEND = 'authz_group.authz_implementation.uw_group_service.UWGroupService'
+
+RESTCLIENTS_ADMIN_GROUP = '{{ restclients_admin_group }}'
+# USERSERVICE_ADMIN_GROUP = '{{ userservice_admin_group }}'
+# USERSERVICE_VALIDATION_MODULE = ''
+# SUPPORTTOOLS_PARENT_APP = "Bridge"
+# SUPPORTTOOLS_PARENT_APP_URL = "/"
+
+BRIDGE_ADMIN_GROUP = '{{ bridge_admin_group }}'
+BRIDGE_IMPORT_CSV_ROOT='/data/bridge/csv'
+BRIDGE_IMPORT_USER_FILENAME='users'
+BRIDGE_IMPORT_USER_FILE_SIZE=30000
+
+RESTCLIENTS_CA_BUNDLE = '{{ base_dir }}/certs/ca-bundle.crt'
+RESTCLIENTS_DAO_CACHE_CLASS='{{restclients_dao_cache_class}}'
+RESTCLIENTS_DISABLE_THREADING = True
+RESTCLIENTS_TIMEOUT = 600
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'bridge': {
+            'format': '%(levelname)-4s %(asctime)s %(message)s [%(name)s]',
+            'datefmt': '%d %H:%M:%S',
+        },
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'bridge': {
+            'level': 'INFO',
+            'class': 'permissions_logging.DateNameFileHandler',
+            'filename': '/data/bridge/logs/bridge-%Y-%m-%d',
+            'permissions': 0o664,
+            'formatter': 'bridge',
+        },
+        'console':{
+            'level': 'ERROR',
+            'class': 'logging.StreamHandler',
+        },
+    },
+   'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins', 'console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'restclients': {
+            'handlers': ['bridge'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'sis_provisioner': {
+            'handlers': ['bridge'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    }
+}
