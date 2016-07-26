166f4db7b866d1034698779490c248d2c9ebb5e4
Fang Lin <fanglin@uw.edu>
2016-07-26 16:57:02 -0700

    Support django 1.9 project/urls.py.

--- playbooks/templates/project_urls.py
+++ playbooks/templates/project_urls.py
@@ -1,10 +1,23 @@
+{% if django19 %}
+from django.conf.urls import include, url
+{% else %}
 from django.conf.urls import patterns, include, url
+{% endif %}
+
 {% if extra_urls_head_section|default(None) %}
 {% include extra_urls_head_section %}
 {% endif %}
 
+{% if django19 %}
+urlpatterns = [
+{% else %}
 urlpatterns = patterns('',
+{% endif %}
     {% for definition in project_url_definitions %}
     {{ definition }},
     {% endfor %}
+{% if django19 %}
+]
+{% else %}
 )
+{% endif %}
