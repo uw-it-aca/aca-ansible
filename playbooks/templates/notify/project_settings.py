dddb8e03cf5fe7923e19d49ec09d50c88b0c32fc
Jim Laney <jlaney@uw.edu>
2017-03-29 13:09:05 -0700

    adds notify project settings template

---
+++ playbooks/templates/notify/project_settings.py
@@ -0,0 +1,16 @@
+# Settings for Notify project.
+TIME_ZONE = 'America/Los_Angeles'
+
+INSTALLED_APPS += [
+    'templatetag_handlebars',
+    'supporttools',
+    'userservice',
+    'authz_group',
+    'notify',
+    'rc_django',
+]
+
+LOGGING = {
+
+
+}
