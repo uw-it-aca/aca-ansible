711c4f13545edc6ab984eba2c7a299509722673e
Craig M. Stimmel <cstimmel@uw.edu>
2017-12-12 15:46:29 -0800

    Trim down settings file.

--- playbooks/templates/pivot/project_settings.py
+++ playbooks/templates/pivot/project_settings.py
@@ -19,30 +19,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '{{ secret_key }}'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = {{ debug }}
-
-ALLOWED_HOSTS = [
-{% for host in allowed_hosts|default([]) %}
-    '{{ host }}',
-{% endfor %}
-]
-
 EMAIL_HOST = '{{ email_host|default('localhost') }}'
 EMAIL_USE_TLS = True
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+INSTALLED_APPS += [
     'pivot',
     'templatetag_handlebars',
 ]
@@ -56,26 +38,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-ROOT_URLCONF = 'project.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
@@ -107,28 +69,5 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.10/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-#STATIC_URL = '/static/'
-
+# Pivot specific settings
 CSV_ROOT = '{{ csv_data_path }}'
-
-LOGIN_REDIRECT_URL = '/'
-
-LOGIN_URL = '/login/'
