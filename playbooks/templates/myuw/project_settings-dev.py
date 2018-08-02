85c31f865a2083b87fa582cde4969785d1255a08
Fang Lin <fanglin@uw.edu>
2018-08-02 13:40:25 -0700

    Add MYUW_DATA_PATH

--- playbooks/templates/myuw/project_settings-dev.py
+++ playbooks/templates/myuw/project_settings-dev.py
@@ -182,6 +182,7 @@ DETECT_USER_AGENTS = {
 GOOGLE_ANALYTICS_KEY = "{{ ga_tracker_key }}"
 GOOGLE_SEARCH_KEY = '{{ gcse_key }}'
 
+MYUW_DATA_PATH = "{{ base_dir }}"
 SERU_LIST = "{{ myuw_seru_path }}"
 
 BLTI_AES_KEY = b'{{ blti_aes_key }}'
