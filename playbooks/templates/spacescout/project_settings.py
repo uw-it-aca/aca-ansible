18422170ee9b0ea9082bf9d53ce6c69a6cc5eef8
Craig M. Stimmel <cstimmel@uw.edu>
2018-10-24 12:12:31 -0700

    spacescout_web settings are no longer needed.

--- playbooks/templates/spacescout/project_settings.py
+++ playbooks/templates/spacescout/project_settings.py
@@ -101,11 +101,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.static',
     'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
-    'spacescout_web.context_processors.show_ios_smart_banner',
-    'spacescout_web.context_processors.less_not_compiled',
-    'spacescout_web.context_processors.is_mobile',
-    'spacescout_web.context_processors.ga_tracking_id',
-    'spacescout_web.context_processors.gmaps_api_key',
 )
 
 CACHES = {
@@ -123,64 +118,18 @@ CACHES = {
 OAUTH_AUTHORIZE_VIEW = 'spotseeker_server.views.oauth.authorize'
 OAUTH_CALLBACK_VIEW = 'spotseeker_server.views.oauth.callback'
 
-MOBILE_USER_AGENTS = 'android|fennec|iemobile|iphone|ipad|opera (?:mini|mobi)'
-
-FEEDBACK_EMAIL_RECIPIENT = ['{{ feedback_email_recip }}']
-DEFAULT_FROM_EMAIL = '{{ from_email }}' # If the user doesn't specify an email address when they report a problem
-SHOW_IOS_SMART_BANNER = True
-
-# For links back to the web app - sharing a space
-SS_APP_SERVER = '{{ spacescout_web_app_hostname }}'
-
+# Required by spacescout_labstats, as well
 SS_WEB_SERVER_HOST = 'http://{{ spacescout_web_app_hostname }}'
 # Run ./manage.py create_consumer on the server and copy the key and secret below. You'll also need to go into the admin and make the oauth client "Trusted."
+# These two required by spacescout_labstats, as well
 SS_WEB_OAUTH_KEY = '{{ spacescout_web_oauth_key }}'
 SS_WEB_OAUTH_SECRET = '{{ spacescout_web_oauth_secret }}'
-
-SS_WEB_LOGOUT_URL = '/user_logout'
-
-DEFAULT_CENTER_LATITUDE = '47.655003'
-DEFAULT_CENTER_LONGITUDE = '-122.306864'
-
-SS_LOCATIONS = {
-    'seattle': {
-        'NAME': 'UW Seattle',
-        'CENTER_LATITUDE': '47.655003',
-        'CENTER_LONGITUDE': '-122.306864',
-        'ZOOM_LEVEL': '15',
-    },
-    'tacoma': {
-        'NAME': 'UW Tacoma',
-        'CENTER_LATITUDE': '47.24579',
-        'CENTER_LONGITUDE': '-122.43737',
-        'ZOOM_LEVEL': '17',
-    },
-    'bothell': {
-        'NAME': 'UW Bothell',
-        'CENTER_LATITUDE': '47.761296',
-        'CENTER_LONGITUDE': '-122.193097',
-        'ZOOM_LEVEL': '16',
-    }
-}
-SS_DEFAULT_LOCATION = 'seattle'
-
-# This is the list of zoom levels for which the spaces are clustered by building on the map.  An empty list means no building clustering
-SS_BUILDING_CLUSTERING_ZOOM_LEVELS = [15, 16, 17]
-
-# The ratio (distance between spaces / diagonal distance visible on map) below which spaces will cluster
-# together on the map when not clustering by building
-SS_DISTANCE_CLUSTERING_RATIO = .04
 
 # Enable sending of email
 EMAIL_HOST = '{{ email_host }}'
 EMAIL_USE_TLS = True
 
-# Enable Google Analytics
-GA_TRACKING_ID = '{{ google_analytics_id }}'
-GMAPS_API_KEY = '{{ google_maps_api_key }}'
-
 MIDDLEWARE_CLASSES = (
-    'spacescout_web.middleware.unpatch_vary.UnpatchVaryMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -190,8 +139,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.RemoteUserMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-    'mobility.middleware.DetectMobileMiddleware',
-    'mobility.middleware.XMobileMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -200,12 +147,8 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.RemoteUserBackend',
 )
 
-
 INSTALLED_APPS += (
     'django.contrib.admin',
-    'spacescout_web',
-    'compressor',
-    'templatetag_handlebars',
     'oauth_provider',
     'spotseeker_server',
     'spacescout_labstats',
@@ -228,16 +171,7 @@ SPOTSEEKER_SPOT_FORM = 'spotseeker_server.org_forms.uw_spot.UWSpotForm'
 SPOTSEEKER_SPOTEXTENDEDINFO_FORM = 'spotseeker_server.org_forms.uw_spot.UWSpotExtendedInfoForm'
 SPOTSEEKER_SEARCH_FILTERS = ['spotseeker_server.org_filters.uw_search.Filter']
 
-# Custom filter for the web app
-SPACESCOUT_SEARCH_FILTERS = ['spacescout_web.org_filters.uw_search.Filter']
-
 OAUTH_PROVIDER_SECRET_SIZE = 64
-
-# admin project settings.py:
-SPACE_TABLE_KEYS = {
-    'FIXED': ('id', 'name',),
-    'SCROLLABLE': ('type',),
-}
 
 INSTALLED_APPS += (
     'django_verbatim',
