c415f59dfb1ff0cbefb36ad91fddc96f8c2d7858
Fang Lin <fanglin@uw.edu>
2018-11-02 19:42:38 -0700

    adjust spacing

--- playbooks/templates/python_saml/project_settings.py
+++ playbooks/templates/python_saml/project_settings.py
@@ -1,9 +1,8 @@
 INSTALLED_APPS += {% if django_version is version_compare('2', '>=') %}[{% else %}({% endif %}
-'uw_saml',
-{% if django_version is version_compare('2', '>=') %}]{% else %}){% endif %}
+'uw_saml',{% if django_version is version_compare('2', '>=') %}]{% else %}){% endif %}
+
 ALLOWED_HOSTS += {% if django_version is version_compare('2', '>=') %}[{% else %}({% endif %}
-'{{ sp_hostname }}',
-{% if django_version is version_compare('2', '>=') %}]{% else %}){% endif %}
+'{{ sp_hostname }}',{% if django_version is version_compare('2', '>=') %}]{% else %}){% endif %}
 
 SAML_USER_ATTRIBUTE = '{{ saml_user_attribute|default("uwnetid") }}'
 
@@ -12,6 +11,7 @@ from django.urls import reverse_lazy
 {% else %}
 from django.core.urlresolvers import reverse_lazy
 {% endif %}
+
 LOGIN_URL = reverse_lazy('saml_login')
 LOGOUT_URL = reverse_lazy('saml_logout')
 
