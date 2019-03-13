65c8c93433af45e52d1803c473270df5438519ae
Craig M. Stimmel <cstimmel@uw.edu>
2019-03-13 14:43:15 -0700

    Remove duplicate app.

--- playbooks/templates/mdot/project_settings.py
+++ playbooks/templates/mdot/project_settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_user_agents',
     'null_command',
     'rest_framework',
     'django_filters',
